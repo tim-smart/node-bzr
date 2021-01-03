d51f9f64d7711f0b9a28eec5c6461c4a099df895
Tim Smart <tim.smart@arisechurch.com>
2021-01-04 05:04:42 +1300

    Add splitLines function

--- src/mod.ts
+++ src/mod.ts
@@ -35,6 +35,12 @@ export const nthColumn = (column: number) => (input: string): string[] =>
     .map((line) => line.trim().split(/\s+/)[column])
     .filter((s) => !!s);
 
+export const splitLines = (input: string) =>
+  input
+    .split("\n")
+    .map((line) => line.trim())
+    .filter((line) => !!line);
+
 const tmpDir = () => Tmp.dir().then((dir) => Path.join(dir.path, "bzr"));
 
 export const branch = (bzr: TBzr, baseDir: string) => async (
@@ -59,7 +65,7 @@ export const tags = (bzr: TBzr) => (args: string[] = ["--sort=time"]) =>
   bzr(["tags", ...args]).then(nthColumn(0));
 
 export const ls = (bzr: TBzr) => (args: string[] = ["-R"]) =>
-  bzr(["ls", ...args]).then((files) => files.split("\n").filter((s) => !!s));
+  bzr(["ls", ...args]).then(splitLines);
 
 export type TBzrWrapper = ReturnType<typeof factory>;
 export default function factory(baseDir: string, opts?: Partial<TOptions>) {
